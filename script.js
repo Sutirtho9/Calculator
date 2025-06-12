c5307d6653aa73a682c80313279f58591eb64c23
Sutirtho9 <sutirthochakravorty@gmail.com>
2025-06-12 08:27:54 +0530

    Delete functionality added

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const display = document.getElementById("display");
+
+const displayArray=[];
 function AppendElement(input){
-    display.value+=input;
+    displayArray.push(input);
+    CreateDisplay();
 }
 function clearDisplay(){
     display.value=" ";
+    displayArray.length=0;
+}
+
+function CreateDisplay() {
+    const display = document.getElementById("display");
+    display.value=" ";
+    for(let i=0;i<displayArray.length;i++){
+        display.value+=displayArray[i];
+    }
+}
+
+function DeleteElement(){
+    displayArray.splice(displayArray.length-1,1);
+    CreateDisplay()
 }
 
 function calculate(){
+    let calc_value=" ";
+    for(let i=0;i<displayArray.length;i++){
+    calc_value+=displayArray[i];
+    }
     try{
-        display.value=eval(display.value);
+        display.value=eval(calc_value);
+
     }
     catch(error){
         display.value="Error"
     }
+
     
     
 
