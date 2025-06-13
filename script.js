acd70eab3d68860a55ec050674d3124386e034f1
Sutirtho9 <sutirthochakravorty@gmail.com>
2025-06-13 09:58:21 +0530

    Keyboard fucntionality added

--- script.js
+++ script.js
@@ -29,13 +29,53 @@ function calculate(){
     }
     try{
         display.value=eval(calc_value);
+        displayArray.length=0;
+        displayArray.push(display.value);
 
     }
     catch(error){
         display.value="Error"
     }
-
-    
-    
-
 }
+
+//Event Listeners
+const opp_buttons = document.querySelectorAll('.opp_btn');
+opp_buttons.forEach(button =>{
+    button.addEventListener('click',()=>{
+    const opp =button.innerText;
+    if(opp==='x'){
+        AppendElement('*')
+    }else{
+        AppendElement(opp); 
+    }
+    });
+})
+
+const del_btn=document.querySelector('.delbtn').
+addEventListener('click',()=>{
+    DeleteElement();
+})
+
+const clear_btn=document.getElementById('cleardisplay').
+addEventListener('click',()=>{
+    clearDisplay();
+})
+
+const calc_btn=document.querySelector('.calc').
+addEventListener('click',()=>{
+    calculate();
+})
+
+document.addEventListener("keydown",(event)=>{
+    const key= event.key;
+    if(!isNaN(key) || "*/+-".includes(key)){
+        AppendElement(key);
+    } else if(key==='Backspace'){
+        DeleteElement();
+    }else if(key==='Enter'){
+        calculate();
+    }
+    else if(key==='Escape'){
+        clearDisplay();
+    }
+});
